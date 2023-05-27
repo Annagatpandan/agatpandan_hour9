4f7a5e62a074a8bc36292039e6ac809243a382c5
Annagatpandan <kissyndelilah6012@gmail.com>
2023-05-27 11:38:47 -0700

    Writing the code for the TriggerScript

--- Assets/scipts/MotionScript.cs
+++ Assets/scipts/MotionScript.cs
@@ -13,6 +13,8 @@ public class MotionScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float mX = Input.GetAxis("Mouse X ") / 10;
+        float mY = Input.GetAxis(" Mouse Y ") / 10;
+        transform.Translate(mX, mY, 0);
     }
 }
--- Assets/scipts/TriggerScript.cs
+++ Assets/scipts/TriggerScript.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using UnitySystem.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,12 +7,22 @@ public class TriggerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        void OnTriggerEnter(Collider other);// is callled when an object enters the trigger 
+        void OnTriggerStay(Collider other); // is called when an object stays in the trigger 
+        void OnTriggerExit(Collider other); // is called when an object exits the trigger    
+        void OnTriggerEnter(Collider other);
     }
+}
+{ 
+    print("Object has entered collider");
+} 
 
+
+}
+void OnTriggerEnter(Collider other);
     // Update is called once per frame
     void Update()
     {
-       
+    print(other.gameObject.Cube + " Cube has entered the trigger");
     }
 }
